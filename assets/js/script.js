7e7e40844789891c96ac97d032e253afdf2a65a4
andyk8872 <andrewkennedy35@yahoo.ie>
2023-05-13 00:40:18 +0000

    added the traditional intro with html and css and the theme using javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,25 @@
 //     audio.play();
 // }
 
-var a = document.getElementById("audio");
-		function playaudio() {
-			a.play();
-		}
-		function pauseaudio() {
-			a.pause();
-		}
-		function stopaudio() {
-			a.pause();
-			a.currentTime = 0;
-        }
+// var a = document.getElementById("audio");
+// 		function playaudio() {
+// 			a.play();
+// 		}
+// 		function pauseaudio() {
+// 			a.pause();
+// 		}
+// 		function stopaudio() {
+// 			a.pause();
+// 			a.currentTime = 0;
+//         }
+
+// window.onload = function () {
+// 	if (localStorage.getItem("hasCodeRunBefore") === null) {
+// 			document.getElementById("audio").play();
+// 		localStorage.setItem("hasCodeRunBefore", true);
+// 	}
+// }	
+
+window.onload = function() {
+	document.getElementById("tah_audio").play();
+}
