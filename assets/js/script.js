063dd303ab86bdb34e815b2b8577d62329b38e78
andyk8872 <andrewkennedy35@yahoo.ie>
2023-05-14 17:04:47 +0000

    Added sound/mute button and refactored

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,17 @@
-// function play() {
-//     var audio = document.getElementById("audio");
-//     audio.play();
-// }
+setTimeout(function(){	
+    	document.getElementById("tah_audio").play();
+    }, 3000) ;
 
-// var a = document.getElementById("audio");
-// 		function playaudio() {
-// 			a.play();
-// 		}
-// 		function pauseaudio() {
-// 			a.pause();
-// 		}
-// 		function stopaudio() {
-// 			a.pause();
-// 			a.currentTime = 0;
-//         }
+	
+const muteButton = document.getElementById('mute');
 
-// window.onload = function () {
-// 	if (localStorage.getItem("hasCodeRunBefore") === null) {
-// 			document.getElementById("audio").play();
-// 		localStorage.setItem("hasCodeRunBefore", true);
-// 	}
-// }	
+function mute(){
+  if(document.getElementById('tah_audio').muted == false){
+	 document.getElementById('tah_audio').muted = true;
+	 muteButton.innerHTML = '<i class="fa-solid fa-volume-xmark"></i>';
+		} else {
+		  document.getElementById('tah_audio').muted = false;
+		  muteButton.innerHTML = '<i class="fa-solid fa-volume-high"></i></i>';
+		}
 
-window.onload = function() {
-	document.getElementById("tah_audio").play();
 }
